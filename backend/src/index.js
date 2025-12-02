b2ea45c43abab1a8e714a220ca687142a23fbbc5
Abuthabu <ahamedbeema1988@gmail.com>
2025-12-02 21:14:13 +0530

    .........

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const searchRoutes = require('./routes/searchRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
